73a552d18dc6f23704b5be3a9eed6093a04ea6ed
Kathleen Law <kathleen.law@acquia.com>
2020-06-03 09:55:58 -0400

    added aws cli deployment command to builder

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -1,27 +1,25 @@
 import { logging } from '@angular-devkit/core';
-import * as fse from 'fs-extra';
 
 import { Schema } from '../deploy/schema';
 
-// TODO: add your deployment code here!
 export async function run(
   dir: string,
   options: Schema,
   logger: logging.LoggerApi
 ) {
   try {
-    // options.targetDir = options.targetDir || '/example-folder';
+    const util = require('util');
+    const exec = util.promisify(require('child_process').exec);
 
-    // if (!(await fse.pathExists(options.targetDir))) {
-    //   throw new Error(`Target directory ${options.targetDir} does not exist!`);
-    // }
+    const bucketPath: string = options.subFolder ? options.subFolder : '';
+    const { stdout } = await exec(
+      `aws s3 sync --acl public-read --delete ${dir} s3://${options.bucket}${bucketPath} --region ${options.region}`
+    );
 
-    // await fse.copy(dir, options.targetDir);
-
-    console.log({ dir, options });
-    logger.info('🚀 Successfully published via nx-deploy-s3! Have a nice day!');
-  } catch (error) {
-    logger.error('❌ An error occurred!');
-    throw error;
+    logger.info(`Successfully deployed application.`);
+    logger.info(stdout);
+  } catch (e) {
+    logger.error(`Failed to deploy application.`);
+    logger.error(e.message);
   }
 }
